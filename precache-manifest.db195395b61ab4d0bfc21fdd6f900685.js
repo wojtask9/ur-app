a7a025990a872334183da2239df3a55f707110cd
wojtask9 <wojtask9.kde@gmail.com>
2018-11-22 11:04:34 +0100

    deploy

---
+++ precache-manifest.db195395b61ab4d0bfc21fdd6f900685.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ur-app/robots.txt"
+  },
+  {
+    "revision": "95588b2e89ed6f2bd155",
+    "url": "/ur-app/js/chunk-vendors.0450473d.js"
+  },
+  {
+    "revision": "5b21df0c664243a45c56",
+    "url": "/ur-app/js/app.c723d53c.js"
+  },
+  {
+    "revision": "e6f8897ae0ef8e5387e8",
+    "url": "/ur-app/js/about.0bf3fb79.js"
+  },
+  {
+    "revision": "ace05053dbc2340fc1598691c53e44d9",
+    "url": "/ur-app/index.html"
+  },
+  {
+    "revision": "95588b2e89ed6f2bd155",
+    "url": "/ur-app/css/chunk-vendors.5b56ea36.css"
+  },
+  {
+    "revision": "5b21df0c664243a45c56",
+    "url": "/ur-app/css/app.216ecc08.css"
+  }
+];
